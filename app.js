8f311b69b88935f18ea610641898e70286a19bd6
Lucas Skog <lucasskog@gmail.com>
2024-02-15 10:44:11 +0100

    Trying to fix this bug..

--- app.js
+++ app.js
@@ -1,60 +1,57 @@
 import { cards } from "./constants.js";
-import { loadShow } from './functions.js';
+import { loadShow } from "./functions.js";
 
 const randomBeerUrl = "https://api.punkapi.com/v2/beers/random";
 
-async function loadRandomBeer(){
-    let randomBeer;
-    randomBeer = await fetchRandomBeer();
-    cards.slider.insertAdjacentElement("beforeend", card(randomBeer))
-    cards.items = document.querySelectorAll(".slider .item");
-    console.log(cards.items.length)
+async function loadRandomBeer() {
+  let randomBeer;
+  randomBeer = await fetchRandomBeer();
+  cards.slider.insertAdjacentElement("beforeend", card(randomBeer));
+  cards.items = document.querySelectorAll(".slider .item");
 }
 
-
 async function fetchRandomBeer() {
-    try {
-        const beerResponse = await fetch(randomBeerUrl);   
-        if(!beerResponse.ok){
-            throw new Error("Response was unsuccesful.")
-        }     
-        const beerData = await beerResponse.json();
-        console.log(beerData[0])
-        return beerData[0];
-    } catch (error) {
-        console.error('Error fetching random beer:', error);
-        throw error;
+  try {
+    const beerResponse = await fetch(randomBeerUrl);
+    if (!beerResponse.ok) {
+      throw new Error("Response was unsuccesful.");
     }
+    const beerData = await beerResponse.json();
+    console.log(beerData[0]);
+    return beerData[0];
+  } catch (error) {
+    console.error("Error fetching random beer:", error);
+    throw error;
+  }
 }
 
 const card = (beer) => {
-const div = document.createElement("div");
-div.classList.add("item");
-div.style.transform = `translateX(${350}px) scale(${1 - 0.2}) perspective(500px) rotateY(180deg)`;
-div.style.color = "black";
-div.style.opacity = 0;
-const name = document.createElement("h1");
-name.textContent = beer.name;
-const imgDiv = document.createElement("div");
-imgDiv.classList.add("img-container");
-const img = document.createElement("img");
-img.src= beer.image_url ?? "img/beer.png";
-imgDiv.appendChild(img);
-div.append(name, imgDiv);
-return div;
-}
+  const div = document.createElement("div");
+  div.classList.add("item");
+  div.style.transform = `translateX(${350}px) scale(${
+    1 - 0.2
+  }) perspective(500px) rotateY(180deg)`;
+  div.style.color = "black";
+  div.style.opacity = 0;
+  const name = document.createElement("h1");
+  name.textContent = beer.name;
+  const imgDiv = document.createElement("div");
+  imgDiv.classList.add("img-container");
+  const img = document.createElement("img");
+  img.src = beer.image_url ?? "img/beer.png";
+  imgDiv.appendChild(img);
+  div.append(name, imgDiv);
+  return div;
+};
 
-loadRandomBeer()
+loadRandomBeer();
 // Card slider functionality
-
-
 
 /**
  * Represents the index of the currently active item.
  * @type {number}
  */
 let active = 0;
-
 
 loadShow(cards.items, active);
 
@@ -63,16 +60,16 @@ loadShow(cards.items, active);
  * and updating the displayed show.
  */
 cards.next.onclick = async function () {
-    if(active === 0){
-        cards.prev.classList.remove("hider")
-    }
-   
-    if(active + 1 === cards.items.length){
-        await loadRandomBeer();
-        setTimeout(loadShow(cards.items, ++active),300)
-    } else {
-        loadShow(cards.items, ++active);
-    }    
+  if (active === 0) {
+    cards.prev.classList.remove("hider");
+  }
+  active++;
+  if (active === cards.items.length) {
+    await loadRandomBeer();
+    setTimeout(loadShow(cards.items, active), 0);
+  } else {
+    loadShow(cards.items, active);
+  }
 };
 
 /**
@@ -80,9 +77,9 @@ cards.next.onclick = async function () {
  * and updating the displayed show.
  */
 cards.prev.onclick = function () {
-    active = active - 1 >= 0 ? active - 1 : active;
-    if(active === 0)
-    cards.prev.classList.add("hider");
-
-    loadShow(cards.items, active);
+    let current = active;
+    active = active === 0 ? 0 : active -1;
+   if (active === 0) cards.prev.classList.add("hider");
+if(current !== active)
+  loadShow(cards.items, active);
 };
